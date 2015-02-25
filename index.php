bbbb8b9b9b4d566a8f3c151eec3b72ae00837eb6
Andrew <andrewjcolescratch@gmail.com>
2015-02-24 20:31:04 -0400

    Created using PullJosh's mockup

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Kudamono Home</title>
+  <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+  
+<body>
+<div class="center">
+  <h1 class="pageTitle">Kudamono</h1>
+  <div class="col-xs-12 col-md-8">
+    <div class="box red">
+      <h3>Lorem Ipsum</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi tempore assumenda facilis, quasi ex aliquam laboriosam fugiat quos ab magni a repellat alias sint, laudantium odio in enim, reiciendis accusantium eum, quod accusamus. Itaque quas iste enim, beatae voluptatum commodi eaque odio quibusdam nisi eum ad ratione odit eos labore ex doloribus dignissimos. Quibusdam eius ducimus placeat dolore accusamus, ex fugit eaque veritatis ipsa illo excepturi aliquam est dolores ipsum necessitatibus id et eum itaque sit quia aliquid vero! Numquam perspiciatis odit dolore repellendus, rem officia temporibus dignissimos aliquam itaque architecto omnis, similique dolor, aut et recusandae dolores enim unde voluptates laudantium ratione neque nam fugit voluptatem. Provident, soluta, natus. Et neque perferendis alias aspernatur!</p>
+     </div>
+   </div>
+  <div class="col-xs-12 col-md-4">
+    <div class="box purple">
+      <h3>Lorem Ipsum</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, omnis blanditiis. Ratione sequi accusantium optio doloremque dolore. Dolorem officiis tempora voluptatibus accusamus veritatis natus, quam suscipit praesentium neque at iusto laboriosam a error ducimus totam dignissimos itaque est, numquam, explicabo doloribus fugit veniam!</p>
+    </div>
+  </div>
+  <div class="col-xs-12 col-md-6">
+    <div class="box blue">
+      <h3>Lorem Ipsum</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, omnis blanditiis. Ratione sequi accusantium optio doloremque dolore. Dolorem officiis tempora voluptatibus accusamus veritatis natus, quam suscipit praesentium neque at iusto laboriosam a error ducimus totam dignissimos itaque est, numquam, explicabo doloribus fugit veniam!</p>
+    </div>
+  </div>
+  <div class="col-xs-12 col-md-6">
+    <div class="box lightBlue">
+      <h3>Lorem Ipsum</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, omnis blanditiis. Ratione sequi accusantium optio doloremque dolore. Dolorem officiis tempora voluptatibus accusamus veritatis natus, quam suscipit praesentium neque at iusto laboriosam a error ducimus totam dignissimos itaque est, numquam, explicabo doloribus fugit veniam!</p>
+    </div>
+  </div>
+  <div class="footer">Made with &hearts; by the scratch community.</div>
+</div>
+</body>
+</html>
+  
